f14a0a3e734bae79bb2616b136127370726a9005
Junghoon Lee <jeonghunn1@gmail.com>
2021-02-11 17:17:49 +0900

    chore: Update angular/core to v11.1.2

--- src/app/itboard/itboard.component.spec.ts
+++ src/app/itboard/itboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ItboardComponent } from './itboard.component';
 
@@ -6,7 +6,7 @@ describe('ItboardComponent', () => {
   let component: ItboardComponent;
   let fixture: ComponentFixture<ItboardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ItboardComponent ],
     })
