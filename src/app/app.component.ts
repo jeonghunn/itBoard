b7aeba09b2faef2dd269167f1bb948e8de47563c
Junghoon Lee <jeonghunn1@gmail.com>
2021-02-13 14:44:32 +0900

    chore: Setup the TranslateService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit,
 } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { ColorSchemeService } from './core/color-scheme.service';
 
 @Component({
@@ -15,7 +16,9 @@ export class AppComponent implements OnInit {
 
   public constructor(
     public colorSchemeService: ColorSchemeService,
+    public translateService: TranslateService,
   ) {
+    translateService.setDefaultLang(translateService.getBrowserLang());
   }
   public ngOnInit(): void {
     this.colorSchemeService.load();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgxChildProcessModule } from 'ngx-childprocess';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { ItboardComponent } from './itboard/itboard.component';
+
+const HttpLoaderFactory = (http: HttpClient) => {
+  return new TranslateHttpLoader(http);
+};
 
 @NgModule({
   declarations: [
@@ -25,6 +37,13 @@ import { ItboardComponent } from './itboard/itboard.component';
     NgxChildProcessModule,
     MatSidenavModule,
     MatCardModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [ HttpClient ],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [ AppComponent ],
