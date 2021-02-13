9e9a8300a9d0e929f25697f9280b1aa3c448e79e
Junghoon Lee <jeonghunn1@gmail.com>
2021-02-13 15:00:18 +0900

    chore: Setup for error pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {
   RouterModule,
   Routes,
 } from '@angular/router';
+import { Error404Component } from './components/error404/error404.component';
 import { ItboardComponent } from './itboard/itboard.component';
 
 const routes: Routes = [
   { path: '', component: ItboardComponent },
   { path: 'main', component: ItboardComponent },
+  { path: '**', component: Error404Component },
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes),
   ],
   exports: [ RouterModule ],
 })
