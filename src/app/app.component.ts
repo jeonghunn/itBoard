8c3b38aa0203680a77c67200ee9cccb140459c7e
Junghoon Lee <jeonghunn1@gmail.com>
2021-02-13 13:42:19 +0900

    chore: Setup for dark mode users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import { ColorSchemeService } from './core/color-scheme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
-  public title: string = 'itboard-front';
+export class AppComponent implements OnInit {
+  public title: string = 'itBoard';
   public isSideNavOpen: boolean = false;
+
+  public constructor(
+    public colorSchemeService: ColorSchemeService,
+  ) {
+  }
+  public ngOnInit(): void {
+    this.colorSchemeService.load();
+  }
 }
---
+++ src/app/core/color-scheme.service.ts
@@ -0,0 +1,67 @@
+import {
+  Injectable,
+  Renderer2,
+  RendererFactory2,
+} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ColorSchemeService {
+
+  private renderer: Renderer2;
+  private colorScheme: string;
+  // Define prefix for clearer and more readable class names in scss files
+  private colorSchemePrefix: string = 'color-scheme-';
+
+  public constructor(rendererFactory: RendererFactory2) {
+    // Create new renderer from renderFactory, to make it possible to use renderer2 in a service
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  public _detectPrefersColorScheme(): void {
+    // Detect if prefers-color-scheme is supported
+    if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+      // Set colorScheme to Dark if prefers-color-scheme is dark. Otherwise set to light.
+      this.colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    } else {
+      // If the browser doesn't support prefers-color-scheme, set it as default to dark
+      this.colorScheme = 'dark';
+    }
+  }
+
+  public _setColorScheme(scheme: string): void {
+    this.colorScheme = scheme;
+    // Save prefers-color-scheme to localStorage
+    localStorage.setItem('prefers-color', scheme);
+  }
+
+  public _getColorScheme(): void {
+    // Check if any prefers-color-scheme is stored in localStorage
+    if (localStorage.getItem('prefers-color')) {
+      // Save prefers-color-scheme from localStorage
+      this.colorScheme = localStorage.getItem('prefers-color');
+    } else {
+      // If no prefers-color-scheme is stored in localStorage, try to detect OS default prefers-color-scheme
+      this._detectPrefersColorScheme();
+    }
+  }
+
+  public load(): void {
+    this._getColorScheme();
+    this.renderer.addClass(document.body, this.colorSchemePrefix + this.colorScheme);
+  }
+
+  public update(scheme: string): void {
+    this._setColorScheme(scheme);
+    // Remove the old color-scheme class
+    this.renderer.removeClass( document.body, this.colorSchemePrefix + (this.colorScheme === 'dark' ? 'light' : 'dark') );
+    // Add the new / current color-scheme class
+    this.renderer.addClass(document.body, this.colorSchemePrefix + scheme);
+  }
+
+  public currentActive(): string {
+    return this.colorScheme;
+  }
+
+}
